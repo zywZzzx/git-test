7fed5e5f1847f3a831fc197a2581174967149b6a
zyw <zyw@qq.com>
2022-07-11 15:28:27 +0800

    second commit

--- src/main/java/com/zyw/git/Git_Test.java
+++ src/main/java/com/zyw/git/Git_Test.java
@@ -3,5 +3,6 @@ package com.zyw.git;
 public class Git_Test {
     public static void main(String[] args) {
         System.out.println("hello git...");
+        System.out.println("hello git222...");
     }
 }
